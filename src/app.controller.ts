3319fc99efbf6f7102302f9b7a5b70729edde8da
YeFei572 <572yefei@163.com>
2025-03-17 21:07:52 +0800

    1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,18 @@
 import { Controller, Get, Logger } from '@nestjs/common';
-import { AppService } from './app.service';
-import { RedisService } from './redis/redis.service';
+import { RedisClusterService } from './redis/redis-cluster.service';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
   constructor(
-    private readonly appService: AppService,
-    private readonly redisService: RedisService,
+    private readonly redisClusterService: RedisClusterService,
   ) { }
 
   @Get('test')
   getHello(): any {
     for (let i = 0; i < 100; i++) {
-      this.redisService.set('test' + i, i.toString());
+      this.redisClusterService.set('test' + i, i.toString());
     }
-    return this.redisService.get('test');
+    return this.redisClusterService.get('test');
   }
 }
--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
-import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
-import { RedisService } from './redis.service';
+// import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule } from '@nestjs/config';
+import redisConfig from 'src/config/redis.config';
+import { RedisClusterService } from './redis-cluster.service';
 
 @Module({
   imports: [
-    NestRedisModule.forRootAsync({
-      useFactory: () => ({
-        type: 'cluster',
-        nodes: [
-          {
-            host: process.env.REDIS_HOST,
-            port: parseInt(process.env.REDIS_PORT),
-            password: process.env.REDIS_PASSWORD,
-            db: 0,
-          }
-        ]
-      })
-    })
+    // NestRedisModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'cluster',
+    //     nodes: [
+    //       {
+    //         host: process.env.REDIS_HOST,
+    //         port: parseInt(process.env.REDIS_PORT),
+    //         password: process.env.REDIS_PASSWORD,
+    //         db: 0,
+    //       }
+    //     ]
+    //   })
+    // })
+    ConfigModule.forFeature(redisConfig)
   ],
-  providers: [RedisService],
-  exports: [RedisService],
+  providers: [RedisClusterService],
+  exports: [RedisClusterService],
 })
 export class RedisModule { }
 
